e32cc5b3f6b34edfabb0c1166e18cf8a6d12d3c7
akshithkannan <158706877+akshithkannan@users.noreply.github.com>
2024-10-06 19:08:16 +0530

    Add files via upload

---
+++ stuckatone.v
@@ -0,0 +1,41 @@
+// Code your design here
+module stuckatone #(
+    parameter N = 32
+)(
+    input clk,
+    input reset,
+    input [N-1:0] data_in,
+    input load,
+    output done,
+    output reg [N-1:0] data_out
+);
+// Add your code here
+  
+  
+  
+  wire [N-1:0]data;
+  wire [N-1:0]mux1;
+  wire [N-1:0]mux2;
+  wire en;
+  
+  assign mux1=(data[0])?(data+(data<<1)+1):(data>>1);
+  
+  assign mux2=load?(data_in):(mux1);
+  
+  assign done=(data==1);
+  
+  assign en=(~(done))|(load);
+  
+  assign data=data_out;
+  
+    
+  always@(posedge clk) begin 
+    if(reset)
+      data_out<=0;
+    else if(en)
+      data_out<=mux2;
+    
+
+  end   
+
+endmodule
